49caf98eb5e943e87f58f57aa7e3b777fc4d6087
Your Name <you@example.com>
2021-07-09 01:48:37 +0700

    Design trello Frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,112 @@
-import './App.css'
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="hello-world">Hello World</h1>
+    <div className="trello-app">
+		<nav className="navbar app">App bar</nav>
+		<nav className="navbar board">Board bar</nav>
+		<div className="board-columns">
+			<div className="column">
+				<header>Brainstorm</header>
+				<ul>
+					<li>
+						<img src="https://images.unsplash.com/photo-1625737186162-5766a7289b5a?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="devphan-alt-img" />
+						Title: Dev Phan
+					</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+				</ul>
+				<footer>Add anothor card</footer>
+			</div>
+			<div className="column">
+				<header>Brainstorm</header>
+				<ul>
+					<li>
+						<img src="https://images.unsplash.com/photo-1593642532781-03e79bf5bec2?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="devphan-alt-img" />
+						Title: Dev Phan
+					</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+				</ul>
+				<footer>Add anothor card</footer>
+			</div>
+			<div className="column">
+				<header>Brainstorm</header>
+				<ul>
+					<li>
+						<img src="https://images.unsplash.com/photo-1593642532781-03e79bf5bec2?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="devphan-alt-img" />
+						Title: Dev Phan
+					</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+				</ul>
+				<footer>Add anothor card</footer>
+			</div>
+			<div className="column">
+				<header>Brainstorm</header>
+				<ul>
+					<li>
+						<img src="https://images.unsplash.com/photo-1593642532781-03e79bf5bec2?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="devphan-alt-img" />
+						Title: Dev Phan
+					</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+				</ul>
+				<footer>Add anothor card</footer>
+			</div>
+			<div className="column">
+				<header>Brainstorm</header>
+				<ul>
+					<li>
+						<img src="https://images.unsplash.com/photo-1593642532781-03e79bf5bec2?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="devphan-alt-img" />
+						Title: Dev Phan
+					</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+				</ul>
+				<footer>Add anothor card</footer>
+			</div>
+			<div className="column">
+				<header>Brainstorm</header>
+				<ul>
+					<li>
+						<img src="https://images.unsplash.com/photo-1593642532781-03e79bf5bec2?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="devphan-alt-img" />
+						Title: Dev Phan
+					</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+				</ul>
+				<footer>Add anothor card</footer>
+			</div>
+			<div className="column">
+				<header>Brainstorm</header>
+				<ul>
+					<li>
+						<img src="https://images.unsplash.com/photo-1593642532781-03e79bf5bec2?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="devphan-alt-img" />
+						Title: Dev Phan
+					</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+					<li>Add what youd like to work on below</li>
+				</ul>
+				<footer>Add anothor card</footer>
+			</div>
+		</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
